5624c6911fdb80749433c02eef97fdc7b3ccbd5c
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-12 11:54:18 -0400

    dashboard count is updated

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
     @category_count = Category.count
+    @products = Product.order(id: :desc).all
   end
 end
