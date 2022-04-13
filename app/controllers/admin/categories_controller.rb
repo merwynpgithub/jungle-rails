d7496675f7807d843c27cf8cb8ef23cedfbe8132
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-13 13:18:05 -0400

    add admin-categories route

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
+
+  def index
+    @categories = Category.all
+  end
+
 end
