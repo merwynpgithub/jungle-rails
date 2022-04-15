e7b49ca808bf9b94e34c21470d8b2927f33c5d16
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-15 16:27:52 -0400

    add current user method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,13 @@ class ApplicationController < ActionController::Base
   end
   helper_method :show_orders_for_id
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
 end
