e9a058a497718716a0102c4c9ae2d6d61bd71e14
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-04-26 14:27:22 -0400

    refactor product name validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe Product, type: :model do
     end
 
     it 'should validate product name' do
-      product_name = Product.new(name: nil)
-      expect(product_name).to_not be_valid
+      product = Product.new(name: nil)
+      expect(product).to_not be_valid
     end
 
     it 'should validate product price' do
